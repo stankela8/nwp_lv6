e39f8584b00304f3324b0e1e7eb272ec82389099
stankela8 <lukastankovic1155@gmail.com>
2025-12-15 11:36:12 +0100

    drugi commit

--- app.js
+++ app.js
@@ -3,37 +3,51 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+var projectsRouter = require('./routes/projects');
 
 var app = express();
 
-// view engine setup
+// MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/projectsdb')
+    .then(() => console.log("✔ MongoDB connected"))
+    .catch(err => console.error(err));
+
+// view engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.set('layout', 'layout'); // koristi views/layout.ejs
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(expressLayouts);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// routes
+app.use('/projects', projectsRouter);
 
-// catch 404 and forward to error handler
+// Redirect root to /projects
+app.get('/', (req, res) => {
+  res.redirect('/projects');
+});
+
+// catch 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
